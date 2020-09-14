e6c63142ad59a9b751f3e922e83e3e82512eb12b
Stanislav Sitnikov <sit.stas14@mail.ru>
2020-09-14 12:18:23 +0900

    first commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int s = Convert.ToInt32(Console.ReadLine());
+            double sk;
+            t = t * 60;
+            s = s * 1000;
+            sk = s / t;
+            Console.WriteLine(sk);
+            Console.ReadKey();
+        }
+    }
+}
